f5983042e0b42811efac79927e7f0a1762651dd7
Anton <mordbergak@gmail.com>
2016-11-02 03:54:33 +0300

    wc now can open files

--- lab-bash/src/main/java/ru/mit/spbau/antonpp/bash/execution/builtin/WordCount.java
+++ lab-bash/src/main/java/ru/mit/spbau/antonpp/bash/execution/builtin/WordCount.java
@@ -6,7 +6,7 @@ import ru.mit.spbau.antonpp.bash.cli.Environment;
 import ru.mit.spbau.antonpp.bash.exceptions.TooManyArgumentsException;
 import ru.mit.spbau.antonpp.bash.io.IOStreams;
 
-import java.io.ByteArrayOutputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.util.List;
 
@@ -17,19 +17,29 @@ import java.util.List;
 public class WordCount extends AbstractBuiltinExecutable {
     @Override
     public int execute(Environment env, List<String> args, IOStreams io) throws Exception {
-        if (args.size() > 0) {
-            throw new TooManyArgumentsException(args.size(), 0);
+        if (args.size() > 1) {
+            throw new TooManyArgumentsException(args.size(), 1);
         }
+        if (args.size() == 1) {
+            try (InputStream in = new FileInputStream(args.get(0))) {
+                wc(in, io.getOut());
+            }
+        } else {
+            wc(io.getIn(), io.getOut());
+        }
+        return 0;
+    }
+
+    private void wc(InputStream in, OutputStream out) throws IOException {
         try (ByteArrayOutputStream bos = new ByteArrayOutputStream(1024)) {
-            ByteStreams.copy(io.getIn(), bos);
+            ByteStreams.copy(in, bos);
             val bytes = bos.toByteArray();
             val text = new String(bytes, Charset.defaultCharset());
             int length = bytes.length;
             int lines = text.split("\n").length;
             int words = text.split("\\s+").length;
             val result = String.format("%d %d %d", lines, words, length);
-            writeString(io, result);
+            writeString(out, result);
         }
-        return 0;
     }
 }
